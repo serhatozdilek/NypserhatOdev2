1a08854a5b7284b4d53c4122f84ae11a2ca865cf
serhatozdilek <132130044@ogr.uludag.edu.tr>
2022-11-02 17:13:32 +0300

    NypSerhatOdev2
    nyp giris ekrani odevi-serhat ozdilek

---
+++ serhatodevv2/WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form2 f2 = new Form2();
+            string kullaniciAdi, sifre;
+            kullaniciAdi = textBox1.Text;
+            sifre = textBox2.Text;
+
+            if (kullaniciAdi == "serhatozdilek" && sifre == "serhat")
+            {
+                f2.Show();
+                MessageBox.Show("Başarılı giriş yaptınız.");
+            }
+            else
+            {
+                MessageBox.Show("Kullanıcı adı veya şifre hatalı. Tekrar deneyin.");
+            }
+        }
+    }
+}
